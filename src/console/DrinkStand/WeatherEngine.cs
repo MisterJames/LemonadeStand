d8f543db1a2894572d6e5d2d822457307abed40d
James Chambers <james@jameschambers.com>
2022-03-14 17:23:31 -0500

    refactoring and tests
    - refactored some code into distinct methods
    - improved comments and explanations
    - added test coverage for new methods

--- src/console/DrinkStand/WeatherEngine.cs
+++ src/console/DrinkStand/WeatherEngine.cs
@@ -4,7 +4,16 @@
     {
         var rnd = new Random(Environment.TickCount);
 
+        // this is a good reason to watch the IntelliSense when
+        // you are coding! when we call 'Next()' on 'Random' the
+        // first number is the INCLUSIVE lower bounds, meaning
+        // the LOWEST number that can be randomly selected. the
+        // second parameter, however, is the EXCLUSIVE uppwer bounds
+        // meaning it CAN'T be that high. so, this is how to 
+        // pick a random number between 1 and 100 that includes
+        // both 1 and 100. 
         var randomWeatherConditions = rnd.Next(1, 101);
+
 
         if (randomWeatherConditions > 95)
             return Weather.Thunderstorms;
@@ -21,6 +30,10 @@
         return Weather.CoolAndCold;
     }
 
+    // weather will affect the sales within a range. even though
+    // it is sunny, it may be cooler, or windy, or humid. to 
+    // reflect that, we add a bit of randomness to the modifier
+    // rather than just using a straight formula
     public static double ComputeWeatherModifier(Weather weather)
     {
         var modifier = 1.0;
@@ -48,7 +61,8 @@
                 break;
         }
 
-        modifier = Math.Max(modifier, 0.15);
+        // we can't go below 0...no refunds, people!
+        modifier = Math.Max(modifier, 0);
 
         Console.WriteLine($"Weather was {weather} with {modifier} modifier.");
 
--- src/console/DrinkStandTests/GameEngineTests.cs
+++ src/console/DrinkStandTests/GameEngineTests.cs
@@ -4,19 +4,78 @@ using Xunit;
 
 namespace DrinkStandTests
 {
-    public class GameEngineTests
+    public class GameEngineTestsShouldComputeExpected
     {
         [Theory]
         [ClassData(typeof(PriceModifierTestData))]
-        public void ExpectedValuesShouldBeCalculated(double expected, int price)
+        public void PriceModifiers(double expected, int price)
         {
-            // price 50 = sales 49
-            // price 46 = sales 50
 
-            Assert.Equal(expected, GameEngine.ComputePriceModifier(price));
+            Assert.Equal(expected, GameEngine.ComputePriceModifier(price), 2);
 
         }
+
+        [Theory]
+        [ClassData(typeof(SignModifierTestData))]
+        public void SignModifiers(double expected, double signCount)
+        {
+            Assert.Equal(expected, GameEngine.ComputeSignsModifier(signCount), 2);
+        }
+
+        [Fact]
+        public void ToleratedRandomRange()
+        {
+            // testing random is hard, but 
+            for (double i = 0; i < 100; i++)
+            {
+                Assert.InRange(
+                    GameEngine.RandomizeModifier(i), 
+                    i - 0.33, 
+                    i + 0.33);
+            }
+        }
+
+        [Fact]
+        public void DiscountedPrice()
+        {
+            // we get a discount on the first three days
+            Assert.Equal(
+                GameEngine.NormalDrinkCost - (GameEngine.NormalDrinkCost * GameEngine.FreeSugarDiscount), 
+                GameEngine.GetCurrentDrinkCost(2));
+        }
+
+        [Fact]
+        public void RegularPrice()
+        {
+            // we get a discount on the first three days
+            Assert.Equal(
+                GameEngine.NormalDrinkCost, 
+                GameEngine.GetCurrentDrinkCost(4));
+        }
+
     }
+}
+
+public class SignModifierTestData : IEnumerable<object[]>
+{
+    // to get these values, I used the desmos calculator
+    // to graph the function, then pulled "easy" values out
+    // so that I could write the function against them
+    //
+    // https://www.desmos.com/calculator/kl9uxpviwz
+
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        // expected, price
+        yield return new object[] { .85, 0 };
+        yield return new object[] { 1, 1 };
+        yield return new object[] { 1.7, 5 };
+        yield return new object[] { 2, 10 };
+        yield return new object[] { 2, 11 };
+        yield return new object[] { 2, 100 };
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
 
 public class PriceModifierTestData : IEnumerable<object[]>
@@ -30,14 +89,14 @@ public class PriceModifierTestData : IEnumerable<object[]>
     public IEnumerator<object[]> GetEnumerator()
     {
         // expected, price
-        yield return new object[] { 100, 1 };
-        yield return new object[] { 100, 13 };
-        yield return new object[] { 80.875, 20 };
-        yield return new object[] { 50, 46 };
-        yield return new object[] { 62, 30 };
-        yield return new object[] { 35.125, 80 };
-        yield return new object[] { 0.125, 96 };
-        yield return new object[] { 0, 100 };
+        yield return new object[] { 2, 1 };
+        yield return new object[] { 2, 13 };
+        yield return new object[] { 1.80875, 20 };
+        yield return new object[] { 1.50, 46 };
+        yield return new object[] { 1.62, 30 };
+        yield return new object[] { 1.35125, 80 };
+        yield return new object[] { 1.00125, 96 };
+        yield return new object[] { 1, 100 };
     }
 
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
